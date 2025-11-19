844996323a3cef0ad2fba7588ac63a6f630ca632
MatteoBraida-ITS <matteo.braida@allievi.itsdigitalacademy.com>
2025-11-19 21:27:23 +0100

    Esercizio 12 fine

--- Librerie/Esercizio 12 librerie.py
+++ Librerie/Esercizio 12 librerie.py
@@ -7,6 +7,17 @@ logs_path = 'logs'
 giorni_pulizia = 7
 current_time = time.time()
 
+data_odierna = datetime.now().strftime("%Y-%m-%d")
+log_filename = os.path.join(logs_path, f'{data_odierna}.log')
+
+logging.basicConfig(
+    format='%(levelname)s - %(asctime)s - %(message)s',
+    filename=log_filename,
+    encoding='utf-8',
+    level=logging.INFO,
+    datefmt='%H:%M:%S'
+)
+
 if os.path.isdir('logs'):
     pass
 else:
@@ -14,10 +25,6 @@ else:
     os.makedirs('logs')
     print("Cartella 'logs' creata!")
 
-tempo = os.path.getmtime(logs_path)
-timestamp = datetime.fromtimestamp(tempo).strftime("%Y-%m-%d")
-
-logging.basicConfig(format='%(asctime)s %(message)s', filename=f'logs/test-{timestamp}.log', encoding='utf-8', level=logging.INFO, datefmt='%H-%M-%S')
 logging.info(f"INFO - Programma avviato")
 logging.info(f"INFO - Utente 'Mario' ha fatto il login" )
 logging.warning(f"WARNING - Memoria bassa (80%)")
@@ -25,11 +32,23 @@ logging.error(f"ERROR - Connessione al database fallita")
 logging.info(f"INFO - Riprovando connessione")
 logging.info(f"INFO - Conessione riuscita!")
 
-print("Cancellazione log più vecchi di 7 giorni...")
-for f in os.listdir(logs_path):
-    data_creazione = os.path.getctime(f)
-    if (current_time - data_creazione) // (24 * 3600) >= 7:
-        os.unlink(f)
-        print('{} removed'.format(f))
-    else:
-        print("(Nessun file da cancellare)")
+print(F"Log scritti in: {log_filename}")
+
+print(f"\nCancellazione log più vecchi di {giorni_pulizia} giorni...")
+file_rimossi = 0
+
+for filename in os.listdir(logs_path):
+    filepath = os.path.join(logs_path, filename)
+
+data_creazione = os.path.getctime(filepath)
+giorni_trascorsi = (current_time - data_creazione) / (24 * 3600)
+
+if giorni_trascorsi >= giorni_pulizia:
+    os.unlink(filepath)
+    print(f"Rimosso {filename} perchè piu vecchio di 7 giorni.")
+    file_rimossi += 1
+
+if file_rimossi == 0:
+    print("Nessun file da cancellare")
+else:
+    print(f"\nFile totali rimossi: {file_rimossi}")
